9b35f94909bfce48332eecca28afb35b72fa65fe
Kevin <kzong@stuy.edu>
2019-02-04 11:01:37 -0500

    Finished and tested

--- makeAllWords.java
+++ makeAllWords.java
@@ -3,23 +3,23 @@ import java.util.ArrayList;
 public class makeAllWords{
 
 
-  public static ArrayList<String> makeAllWords(int k){
+  public static ArrayList<String> makeAllWords(int k, int maxLetter){
     ArrayList<String> data = new ArrayList<String>();
-    makeAllWordsH(data, k, "");
+    makeAllWordsH(data, k, "", maxLetter);
     return data;
   }
 
-  public static void makeAllWordsH(ArrayList<String> data, int k, String s){
+  public static void makeAllWordsH(ArrayList<String> data, int k, String s, int maxLetter){
     if (k == 0) data.add(s);
     else{
-      makeAllWordsH(data, k-1, s + "a");
-      makeAllWordsH(data, k-1, s + "b");
-      makeAllWordsH(data, k-1, s + "c");
+      for (int i = 97; i < 97 + maxLetter; i++){
+        makeAllWordsH(data, k-1, s + (char)i, maxLetter);
       }
+    }
   }
 
   public static void main(String[] args){
-    System.out.println(makeAllWords(2));
+    System.out.println(makeAllWords(4,7));
   }
 
 
